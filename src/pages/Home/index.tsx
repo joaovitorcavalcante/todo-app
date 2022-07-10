9b72c021d090f5a2cf5e1001050023ed0bc069c7
João <117310@pmenos.com.br>
2022-07-09 23:43:20 -0300

    adding mock tasks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,8 +10,41 @@ type Task = {
   isCompleted: boolean;
 };
 
+const tasksMock = [
+  {
+    id: '1-fake',
+    name: 'Complete online JavaScript course',
+    isCompleted: true,
+  },
+  {
+    id: '2-fake',
+    name: 'Jog around the park 3x',
+    isCompleted: false,
+  },
+  {
+    id: '3-fake',
+    name: '10 minutes meditation',
+    isCompleted: false,
+  },
+  {
+    id: '4-fake',
+    name: 'Read for 1 hour',
+    isCompleted: false,
+  },
+  {
+    id: '5-fake',
+    name: 'Pick up groceries',
+    isCompleted: false,
+  },
+  {
+    id: '6-fake',
+    name: 'Complete Todo App on Frontend Mentor',
+    isCompleted: false,
+  },
+];
+
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>(tasksMock);
 
   function addNewTask(task: Task) {
     setTasks([...tasks, task]);
