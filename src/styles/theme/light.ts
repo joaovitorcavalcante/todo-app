e346da3858b5f7d5918a5b95e6649afe536b5aea
João <117310@pmenos.com.br>
2022-07-09 22:33:58 -0300

    TaskList finished

--- src/styles/theme/light.ts
+++ src/styles/theme/light.ts
@@ -4,7 +4,10 @@ export default {
     base: '#FAFAFA',
     primary: '#FFFFFF',
     text: '#494C6B',
+
     checkBorder: '#E3E4F1',
+
     inputPlaceholder: '#9495a5',
+    inputShadow: '#C2C3D6',
   },
 };
